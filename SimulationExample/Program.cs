55a6d95353c45771f8d338904088ca25890d6f4a
andrewc <andrewc@M216-T>
2023-10-17 12:25:58 +0200

    Add project files.

---
+++ SimulationExample/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SimulationExample;
+
+Console.WriteLine("Hello, World!");
+
+// initialise only once!!
+Random r = new Random();
+
+Simulation mySimulator = new Simulation(r);
+
+const int N = 300000;
+
+List<double> valueResults = new List<double>();
+
+for (int i = 0; i < N; i++)
+{
+    var result = mySimulator.Simulate();
+    // Console.WriteLine($"The outcome is {result.Item1}, with a value of {result.Item2}");
+    valueResults.Add(result.Item2);
+}
+
+Console.WriteLine(
+    valueResults.Average());
+
---
+++ SimulationExample/Simulation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationExample
+{
+    internal class Simulation
+    {
+        private Random r;
+        internal Simulation(Random r)
+        {
+            this.r = r;
+        }
+
+        internal (string, float) Simulate()
+        {
+            // Randomly choose between the possible outcomes
+            // calculate the result
+            double randomResult = r.NextDouble();
+
+            if (randomResult < 0.25)
+            {
+                // the result is HH with probability of 0.25
+                return ("HH", 3.5f);
+            } else if (randomResult < 0.5)
+            {
+                // the result is HT with probability of 0.5 - 0.25 = 0.25
+                return ("HT", -1.0f);
+            } else if (randomResult < 0.75)
+            {
+                // the result is TH with probability of 0.75 - 0.5 = 0.25
+                return ("TH", -1.0f);
+            } else
+            {
+                // otherwise, the remaining probability is 0.25
+                return ("TT", -2.0f);
+            }
+            /*
+            char firstResult = r.NextDouble() < 0.5 ? 'H' : 'T';
+            char secondResult = r.NextDouble() < 0.5 ? 'H' : 'T';
+
+            string outcome = String.Concat(firstResult, secondResult);
+            */
+
+
+
+
+            // return the value
+        }
+    }
+}
